0ca9922a32073530dcc39095a480f47adc73c366
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2020-08-06 18:35:25 +0200

    minor refactor

--- PizzaTime/src/main/scala/gameview/fx/FXCreditsScene.scala
+++ PizzaTime/src/main/scala/gameview/fx/FXCreditsScene.scala
@@ -5,7 +5,7 @@ import gameview.scene.Scene
 import javafx.fxml.FXML
 import javafx.scene.control.Button
 
-case class FXCreditsScene (override val windowManager: Window) extends FXView(Some("CreditsScene.fxml")) with Scene {
+case class FXCreditsScene(override val windowManager: Window) extends FXView(Some("CreditsScene.fxml")) with Scene {
   @FXML protected var backButton: Button = _
 
   backButton.setOnMouseClicked(_ => FXWindow.observers.foreach(observer => observer.onBack()))
