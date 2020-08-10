b6e96c67b9aac4cff4bc522f40fee78885c89a7d
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2020-08-10 10:21:57 +0200

    adjusted enemy, player + modified collectible

--- PizzaTime/src/main/scala/gamelogic/Collectible.scala
+++ PizzaTime/src/main/scala/gamelogic/Collectible.scala
@@ -1,5 +1,8 @@
 package gamelogic
 
-trait Collectible extends Entity {
+import utilities.Position
 
-}
+/** A collectible placed on the [[Floor]]. */
+sealed trait Collectible extends Entity
+case class BonusLife(var position: Position) extends Collectible
+case class BonusScore(var position: Position, var value: Int) extends Collectible
