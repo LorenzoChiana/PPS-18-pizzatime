c915488c3e8d25a53ddf678422b29e487c7d1817
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2020-08-14 11:49:04 +0200

    some utility methods for moving entities

--- PizzaTime/src/main/scala/gamelogic/MovableEntity.scala
+++ PizzaTime/src/main/scala/gamelogic/MovableEntity.scala
@@ -1,8 +1,37 @@
 package gamelogic
 
+import utilities.ImplicitConversions._
+import MovableEntity._
+import Arena._
+import utilities.{Direction, Up, Down, Left, Right, Point, Position}
+
 /** An entity that can move.
  *  Implemented by [[Player]] and [[Bullet]].
  */
 trait MovableEntity extends Entity {
+  /** Returns the set of [[Point]] a [[MovableEntity]] can move to. */
+  def surroundings: Set[Point] = {
+    val surroundings: Set[Point] = Set(
+      stepPoint(position.point, Up),
+      stepPoint(position.point, Down),
+      stepPoint(position.point, Left),
+      stepPoint(position.point, Right)
+    )
+    surroundings -- bounds
+  }
+}
 
+/** Utility methods for [[MovableEntity]]. */
+object MovableEntity {
+  /** Returns the adjacent [[Point]] in a given [[Direction]].
+   *
+   *  @param p the starting [[Point]]
+   *  @param dir the [[Direction]] to consider
+   */
+  def stepPoint(p: Point, dir: Direction): Point = dir match {
+    case Up => (p.x, p.y - 1)
+    case Down => (p.x, p.y + 1)
+    case Left => (p.x - 1, p.y)
+    case Right => (p.x + 1, p.y)
+  }
 }
