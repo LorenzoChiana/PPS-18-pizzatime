7d4f0c2b891dffaa11a0d95c0720aa1aef8af8c4
Giada Gibertoni <giada.gibertoni@gmail.com>
2020-09-30 15:40:28 +0200

    Add another enemy

---
+++ PizzaTime/src/main/scala/gamelogic/EnemyWithLeftRightMove.scala
@@ -0,0 +1,5 @@
+package gamelogic
+
+class EnemyWithLeftRightMove {
+
+}
--- PizzaTime/src/main/scala/gamelogic/EnemyWithRandomMove.scala
+++ PizzaTime/src/main/scala/gamelogic/EnemyWithRandomMove.scala
@@ -15,26 +15,37 @@ case class Enemy(id: Int, position: Position, lives: Int) extends LivingEntity w
   val pointsKilling: Int = 20
   private var disableBehavior: Boolean = false
   private val movementRate: Int = 20
-
+  /**
+   * move(X1,Y1,X2,Y2) :- X2 is X1+1, Y2 is Y1.
+   * move(X1,Y1,X2,Y2) :- X2 is X1-1, Y2 is Y1.
+   * move(X1,Y1,X2,Y2) :- Y2 is Y1+1, X2 is X1.
+   * move(X1,Y1,X2,Y2) :- Y2 is Y1-1, X2 is X1.
+   *
+   * %search_point(+List, +H, -B) B true if H is present
+   * search([H|T], Non_walkable_tiles, XY) :- findall(H, member(H, Non_walkable_tiles), R), member(_, R) -> search(T, Non_walkable_tiles, XY) ; XY = H.
+   *
+   * calc_point(X, Y, Non_walkable_tiles, XY) :- findall((X1,Y1), move(X,Y,X1,Y1), R), search(R, Non_walkable_tiles, XY).
+   *
+   */
   import Scala2P._
   val engine: Term => Seq[Option[Point]] = prolog("""
-      move(X1,Y1,X2,Y2) :- X2 is X1+1, Y2 is Y1.
-      move(X1,Y1,X2,Y2) :- X2 is X1-1, Y2 is Y1.
-      move(X1,Y1,X2,Y2) :- Y2 is Y1+1, X2 is X1.
-      move(X1,Y1,X2,Y2) :- Y2 is Y1-1, X2 is X1.
+	    move(X1, Y1, 0, XY) :- X2 is X1+1, XY = (X2,Y1).
+      move(X1, Y1, 1, XY) :- X2 is X1-1, XY = (X2,Y1).
+      move(X1, Y1, 2, XY) :- Y2 is Y1+1, XY = (X1,Y2).
+      move(X1, Y1, 3, XY) :- Y2 is Y1-1, XY = (X1,Y2).
 
-      %search_point(+List, +H, -B) B true if H is present
-      search([H|T], Non_walkable_tiles, XY) :- findall(H, member(H, Non_walkable_tiles), R), member(_, R) -> search(T, Non_walkable_tiles, XY) ; XY = H.
-
-      calc_point(X, Y, Non_walkable_tiles, XY) :- findall((X1,Y1), move(X,Y,X1,Y1), R), search(R, Non_walkable_tiles, XY).
-  """)
+      calc_point(X, Y, Non_walkable_tiles, Point) :- rand_int(4, Rnd), move(X,Y, Rnd, XY), (member(XY, Non_walkable_tiles) -> calc_point(X, Y, Non_walkable_tiles, XY); Point = XY).
+""")
 
   override def movementBehaviour: Option[EnemyCharacter] = {
-    println(engine("calc_point(" + position.point.x + ", " + position.point.y + "," + nonWalkableTiles + ", Point) " ).head.get)
+
     nextInt(movementRate) match {
-      case 0 => { val en = Enemy(id, Position(engine("calc_point(" + position.point.x + ", " + position.point.y + "," + nonWalkableTiles + ", Point) " ).head.get, position.dir), lives)
-        println(en)
-        Some(en)
+      case 0 => {
+        val result = engine("calc_point(" + position.point.x + ", " + position.point.y + "," + nonWalkableTiles + ", Point) " )
+        result.size match {
+          case 0 => Some(Enemy(id, position, lives))
+          case _ => Some(Enemy(id, Position(result.head.get,position.dir),lives))
+        }
       }
       case _ => None
     }
